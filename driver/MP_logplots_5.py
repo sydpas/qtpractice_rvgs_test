916b2a220767c0f7053ce66ce22cf8a93509adce
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-06-16 15:46:34 -0600

    screwing around

--- driver/MP_logplots_5.py
+++ driver/MP_logplots_5.py
@@ -49,9 +49,6 @@ class WellLogPlotter(FigureCanvas):
         self.plot_horizontal_well()
 
         self.title_func()
-        # self.scale_bar_md()
-        # self.scale_bar_axes = None
-        # self.scale_bar()
 
     def plotting_logs(self):
         """
@@ -63,7 +60,8 @@ class WellLogPlotter(FigureCanvas):
         ax_list, col_list = organize_curves()
 
         self.fig.clear()
-        self.axes = self.fig.subplots(1, len(ax_list), sharey = True, gridspec_kw={'width_ratios': [1, 2, 1, 2, 2]})
+        self.axes = self.fig.subplots(1, len(ax_list), sharey = True,
+                                      gridspec_kw={'width_ratios': [1, 2, 1, 2, 2]})
         # subplots: gridbased, good for shared axes
 
         shade_list = ['blue', 'green', 'orange']
@@ -101,13 +99,10 @@ class WellLogPlotter(FigureCanvas):
                     ax.fill_betweenx(df['SUBSEA'], df[curve], 0, facecolor='white')
                     ax.axvline(75, color='black', linewidth=0.5, alpha=0.5)
 
-                    print(f'x: {curve}, y: {df['SUBSEA']}')
-
                 else:
-
                     shade = shade_list[curve_counter % len(shade_list)]
                     curve_counter += 1
-                    print(f'Shade for {curve}: {shade}')
+                    # print(f'Shade for {curve}: {shade}')
 
                     next_ax = ax2 if j > 0 and ax2 else ax
 
@@ -115,13 +110,23 @@ class WellLogPlotter(FigureCanvas):
                         x=curve, y='SUBSEA', color=shade, ax=next_ax,
                         linewidth=0.5, marker='o', markersize=0.1, alpha=0.3, label=curve)
 
-            # adjusting proper y limits and x limits
+            # adjusting proper y limits
             ax.set_ylim(df['SUBSEA'].min(), df['SUBSEA'].max())
-            print(f'kb for well logs: {kb}')
-            print(f'yaxis min (ss for well logs): {df['SUBSEA'].min()}, max (ss for well logs): {df['SUBSEA'].max()}')
-            ax.set_ylabel('Subsea for Logs (m)')
+
+            if i == len(ax_list) - 1:
+                print(f'kb for well logs: {kb}')
+                print(f'yaxis min (ss for well logs): {df['SUBSEA'].min()}, max (ss for well logs): {df['SUBSEA'].max()}')
+
+            if ax2:
+                ax2.set_ylabel('Subsea for Logs (m)', color='darkred')
+                ax2.tick_params(axis='y', colors='darkred')
+            else:
+                ax.set_ylabel('Subsea for Logs (m)', color='darkred')
+                ax.tick_params(axis='y', colors='darkred')
+
             ax.invert_yaxis()
 
+            # ... and x limits
             ax.set_xlim(df[curves[0]].min(), df[curves[0]].max())
             if ax2:
                 ax2.set_xlim(df[curves[-1]].min(), df[curves[-1]].max())
@@ -146,6 +151,7 @@ class WellLogPlotter(FigureCanvas):
         for pair in self.tops_lines_list:
             pair.set_visible(self.show_tops)
         self.draw()
+
 
     def plot_horizontal_well(self):
         """
@@ -178,30 +184,30 @@ class WellLogPlotter(FigureCanvas):
         self.horz_well_axes.tick_params(axis='y', colors='darkblue')
 
         # now to make sure the well spans the entire plot
-        ymin, ymax = horz_df['SS'].min() - 100, horz_df['SS'].max() + 100
+        ymin, ymax = horz_df['SS'].min(), horz_df['SS'].max()
         print(f'kb for horz well: 824')
         print(f'yaxis min (ss for horz well): {ymin}, max (ss for horz well): {ymax}')
         self.horz_well_axes.set_ylim(ymin, ymax)
 
-        xmin, xmax = horz_df['EW'].min() - 100, horz_df['EW'].max() + 100
+        xmin, xmax = horz_df['EW'].min(), horz_df['EW'].max()
         # print(f'xmin: {xmin}, xmax: {xmax}')
         self.horz_well_axes.set_xlim(xmin, xmax)
 
         self.horz_well_axes.scatter(
             horz_df['EW'], horz_df['SS'],  # x, y
-            color='darkred', marker='.', s=20, label='Horizontal Well')
+            color='brown', marker='.', s=20, label='Horizontal Well')
 
         self.horz_well_axes.axhline(0, 0, 1, color='darkblue', lw=1.5, ls='--', alpha=0.5, label='Sea Level')
 
         # find the point in the well that's closest to a chosen MD
-        target_md = 2000
+        target_md = 1600
         # subtracts target from every MD val, take abs val, find index where val is small, take entire row (iloc)
         closest_point = horz_df.iloc[(horz_df['MD'] - target_md).abs().idxmin()]
 
         # plotting target point
         self.horz_well_axes.scatter(
             closest_point['EW'], closest_point['SS'],
-            color='orange', edgecolors='red', marker='^', s=40, label='Target Point')
+            color='orange', edgecolors='brown', marker='X', s=40, label='Target Point')
 
         self.horz_well_axes.legend(loc='upper right', fontsize=7)
 
@@ -213,123 +219,6 @@ class WellLogPlotter(FigureCanvas):
         plt.suptitle(f'Horizontal Well ({uwi_title}) on\n {loc} for {comp}\n +{kb:.2f}m', fontsize=8, fontweight='bold',
                      bbox=dict(facecolor='lightblue', edgecolor='black', boxstyle='square,pad=0.6', alpha=0.8))
 
-
-    # def scale_bar_md(self):
-    #     """
-    #     This function creates a scale bar to overlay the plots.
-    #     """
-    #     columns, non_depth_curves, curve_unit_list, df, loc, comp, kb = mainpass_well()
-    #     horz_df = horz_loader()
-    #
-    #     self.scale_bar_axes = self.fig.add_axes((0.125, 0.109, 0.774, 0.77), sharey=self.axes[0])  # l b width height
-    #
-    #     # add_axes: good for manual locations
-    #     self.scale_bar_axes.set_navigate(True)
-    #
-    #     # make transparent background
-    #     self.scale_bar_axes.patch.set_alpha(0)
-    #
-    #     self.scale_bar_axes.set_xlabel('')
-    #     self.scale_bar_axes.set_xticks([])
-    #     self.scale_bar_axes.set_ylabel('')
-    #     self.scale_bar_axes.set_yticks([])
-    #
-    #     # get rid of window outline
-    #     for spine in self.scale_bar_axes.spines.values():
-    #         spine.set_alpha(0)
-    #
-    #     self.scale_bar_axes.set_xlim(0, horz_df['MD'].max())
-    #
-    #     midpoint = (df['DEPTH'].max() - df['DEPTH'].min()) /2
-    #     mp_pos_1 = midpoint + 1
-    #     mp_neg_1 = midpoint - 1
-    #
-    #     self.horz_lines_list.append(self.scale_bar_axes.axhline(y=midpoint, xmin=0, xmax=1, color='black', linewidth = 2))
-    #     self.vert_lines_list.append(self.scale_bar_axes.axvline(0, mp_neg_1, mp_pos_1, color='black', linewidth = 3))
-    #
-    #     count = 0
-    #     while count <= horz_df['MD'].max():
-    #         self.vert_lines_list.append(self.scale_bar_axes.axvline(count, 0.49, 0.51, color='purple',
-    #                                                                 linewidth=2))
-    #         self.text_lines_list.append(self.scale_bar_axes.text(count, (midpoint + 30), f'{count} m',
-    #                                                     va='top', ha='center', color='purple'))
-    #
-    #         count += 500
-    #
-    #     self.vert_lines_list.append(self.scale_bar_axes.axvline(horz_df['MD'].max(), 0.49, 0.51,
-    #                                                    color='black', linewidth = 3))
-    #
-    #     self.text_lines_list.append(self.scale_bar_axes.text(horz_df['MD'].max() + 10, (midpoint + 20),
-    #                                                          f'{horz_df['MD'].max()} m'))
-    #     self.text_lines_list.append(self.scale_bar_axes.text(horz_df['MD'].max() + 10, (midpoint - 30), 'MD'))
-    #
-    # def toggle_scale_bar(self):
-    #     self.show_scale_bar = not self.show_scale_bar
-    #     for i in [self.vert_lines_list, self.horz_lines_list, self.text_lines_list]:
-    #         for j in i:
-    #             j.set_visible(self.show_scale_bar)
-    #     self.draw()
-
-    # def scale_bar(self):
-    #     columns, non_depth_curves, curve_unit_list, df, loc, comp, kb = mainpass_well()
-    #     horz_df = horz_loader()
-    #
-    #     d_ymin, d_ymax = df['DEPTH'].min(), df['DEPTH'].max()
-    #     d_diff = d_ymax - d_ymin
-    #
-    #     s_ymin, s_ymax = horz_df['SS'].min(), horz_df['SS'].max()
-    #     s_diff = s_ymax - s_ymin
-    #
-    #     print(f'yaxis min (depth): {d_ymin}, max (depth): {d_ymax}')
-    #     print(f'yaxis min (ss): {s_ymin}, max (ss): {s_ymax}')
-    #     print(f'depth difference: {d_diff} meters')
-    #     print(f'subsea difference: {s_diff} meters')
-    #
-    #     # creating a new overlay for the scale bar
-    #     self.scale_bar_axes = self.fig.add_axes((0.125, 0.109, 0.774, 0.77), sharey = None)  # l b width height
-    #     self.scale_bar_axes.set_navigate(False)
-    #
-    #     # make transparent background
-    #     self.scale_bar_axes.patch.set_alpha(0)
-    #
-    #     self.scale_bar_axes.set_xlabel('')
-    #     self.scale_bar_axes.set_xticks([])
-    #     self.scale_bar_axes.set_ylabel('')
-    #     self.scale_bar_axes.set_yticks([])
-    #
-    #     # get rid of window outline
-    #     for spine in self.scale_bar_axes.spines.values():
-    #         spine.set_alpha(0)
-    #
-    #     self.scale_bar_axes.set_xlim(0, horz_df['MD'].max())
-    #
-    #     self.scale_bar_axes.axhline(y=0.5, xmin=0, xmax=1, color='black', linewidth = 2)  # horz line
-    #     self.scale_bar_axes.axvline(0, 0.49, 0.51, color='black', linewidth = 3)
-    #
-    #     ratio_scale = 1
-    #     print('beginning while loop...')
-    #     while (ratio_scale + 100) <= horz_df['SS'].max():
-    #         print(f'ratio scale: {ratio_scale}')
-    #
-    #         math_d_div_ss = (d_diff * ratio_scale) / s_diff
-    #         decimal = math_d_div_ss % 1
-    #         scale_length = math_d_div_ss - decimal
-    #         print(f'scale length: {scale_length} meters')
-    #
-    #         print(f'subsea to depth ratio: {ratio_scale}:{scale_length} meters')
-    #
-    #
-    #         # self.scale_bar_axes.axvline(ratio_scale, 0.49, 0.51, color='purple', linewidth = 2)
-    #         # self.scale_bar_axes.text(ratio_scale, 0.51, f'{ratio_scale} m', va='bottom', ha='center', color='purple')
-    #
-    #         self.scale_bar_axes.axvline(scale_length, 0.49, 0.51, color='brown', linewidth = 2)
-    #         self.scale_bar_axes.text(scale_length, 0.49, f'{scale_length} m', va='top', ha='center', color='brown')
-    #
-    #         ratio_scale = ratio_scale + 100
-    #         print(f'updated ratio scale: {ratio_scale} meters')
-    #
-    #     self.scale_bar_axes.axvline(horz_df['SS'].max(), 0.49, 0.51, color='black', linewidth = 3)
-    #     self.scale_bar_axes.text(horz_df['SS'].max() + 10, 0.49, f'{horz_df['SS'].max()} m')
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -356,26 +245,9 @@ class MainWindow(QMainWindow):
         }
         """)
 
-        # # for the scale bar
-        # self.toggle_button_b = QToolButton()
-        # self.toggle_button_b.setText('Toggle Scale Bar')
-        # self.toggle_button_b.clicked.connect(self.canvas.toggle_scale_bar)
-        # self.toggle_button_b.setAutoRaise(False)
-        # # styling the button
-        # self.toggle_button_b.setStyleSheet("""
-        # QToolButton {background-color: purple; color: white; font: bold 12px; border: 2px dark purple;
-        #     border-radius: 4px; padding: 4px;
-        # }
-        #
-        # QToolButton:hover {background-color: black; color: white; font: bold 12px; border: 2px solid purple;
-        #     border-radius: 4px;padding: 4px;
-        # }
-        # """)
-
         layout = QVBoxLayout()
         layout.addWidget(self.toolbar)
         self.toolbar.addWidget(self.toggle_button)  # to put button beside toolbar
-        # self.toolbar.addWidget(self.toggle_button_b)
         layout.addWidget(self.canvas)
 
         # 'wrap' everything
